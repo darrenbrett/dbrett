9d249341b8cdecfd382d0f48cc3570e29b82acac
Darren King <darrenbrettking@hotmail.com>
2016-01-09 11:51:57 -0800

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+
+root 'static#index'
+
+match '/articles',     to: 'static#articles',    via: 'get'
+match '/contact',      to: 'static#contact',     via: 'get'
+match '/article_show', to: 'static#article_show', via: 'get'
+
+end
