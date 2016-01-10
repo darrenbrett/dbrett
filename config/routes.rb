ba07ed72e1c42d5eeb84119a896415020c903066
Darren King <darrenbrettking@hotmail.com>
2016-01-09 21:27:23 -0800

    Fixed interation issue

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,5 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-match '/articles',     to: 'static#articles',    via: 'get'
-match '/contact',      to: 'static#contact',     via: 'get'
-match '/article_show', to: 'static#article_show', via: 'get'
 
 end
